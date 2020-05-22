b2cfa837fef62bead947ec56595ccf54f860b76d
David-Fadida <fadidadavid4@gmail.com>
2020-05-22 15:55:58 +0300

    21.05.2020 - Referee enter screen  + custom select drop list  + java script - in process..

--- src/main/resources/public/webApp.js
+++ src/main/resources/public/webApp.js
@@ -334,3 +334,19 @@ function createGamesCard(cardInfo) {
 }
 
 //</editor-fold>
+
+const selected = document.querySelector(".selectedGame");
+const optionContainer = document.querySelector(".option-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+    optionContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        optionContainer.classList.remove("active");
+    });
+});
