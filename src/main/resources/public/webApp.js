01ce06eac14fd619424ce26c69ccd784fced5edc
David-Fadida <fadidadavid4@gmail.com>
2020-05-28 10:53:30 +0300

    28.05.2020 + Game Manager - Completed + Alerts - Completed

--- src/main/resources/public/webApp.js
+++ src/main/resources/public/webApp.js
@@ -95,6 +95,25 @@ $(document).ready(function () {
 //<editor-fold desc="LOGIN">
 
 // LOGIN function
+const url = 'http://localhost:8080';
+let stompClient;
+let newMessages = new Array();
+
+function connectToChat(userName) {
+    console.log("connecting to chat...");
+    let socket = new SockJS(url + '/chat');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log("connected to: " + frame);
+        stompClient.subscribe("/topic/messages/" + userName, function (response) {
+            console.log("updated!");
+            let data = JSON.parse(response.body);
+            new Alert(data.description, data.date, data.read);
+            window.alert(data.description);
+        });
+    });
+}
+
 $(document).ready(function () {
     $("#loginSubmit").click(function (event) {
         event.preventDefault();
@@ -106,12 +125,16 @@ $(document).ready(function () {
         let SecureObj = new SecurityObj(email, "1000", "Login", signIn);
         postSend("/Login", SecureObj).then(function (data) {
             // data is 'null' when authentication fail
-            if (data == null) changeLayout(false, false, false);
+            if (data == null) {
+                changeLayout(false, false, false);
+                return;
+            }
             for (let i = 0; i < data.object[0].length; i++) {
                 if (data.object[0][i] == 'Fan') fan = true;
                 if (data.object[0][i] == 'Representative') representative = true;
                 if (data.object[0][i] == 'Referee') referee = true;
             }
+            connectToChat(email);
             changeLayout(fan, representative, referee);
         }).catch(function (err) {
             console.log("failed: " + err);
@@ -336,38 +359,61 @@ function createGamesCard(cardInfo) {
 
 //</editor-fold>
 
-const selected = document.querySelector(".selected");
-const optionContainer = document.querySelector(".option-container");
-
-const optionsList = document.querySelectorAll(".option");
-
-selected.addEventListener("click", () => {
-    optionContainer.classList.toggle("active");
-});
-
-optionsList.forEach(o => {
-    o.addEventListener("click", () => {
-        selected.innerHTML = o.querySelector("label").innerHTML;
-        GameOptionID = o.querySelector("label").htmlFor;
-        optionContainer.classList.remove("active");
-    });
-});
-
 function createGameOption(gameOptionInfo) {
     let gameOptionTemplate = [
         '<div class="option">',
         '<input type="radio" class="radio" id="gameOption',
-            gameOptionInfo.id || 'undefined',
+        gameOptionInfo.id || 'undefined',
         '" name="GameOption">',
         '<label for=gameOption',
-            gameOptionInfo.id || 'undefined',
+        gameOptionInfo.id || 'undefined',
         '>',
-            gameOptionInfo.home || 'undefined',
+        gameOptionInfo.home.name || 'undefined',
         ' VS ',
-            gameOptionInfo.away || 'undefined',
+        gameOptionInfo.away.name || 'undefined',
         '</label></div>'
     ];
     return $(gameOptionTemplate.join(''));
+}
+
+let activeGame = false;
+let interval;
+let minute;
+let strip = 10;
+
+$('#StartEnd').click(function () {
+    activeGame = !activeGame;
+    if (activeGame) {
+        document.querySelector('#StartEnd').innerHTML = 'END GAME';
+        $('#addEvent').removeAttr("disabled");
+        $('#sideNavBar').css('pointer-events', 'none');
+        $('#HomeEvents').empty();
+        $('#AwayEvents').empty();
+        minute = 0;
+        strip = 10;
+        document.querySelector('#minute').innerHTML = minute + "'";
+        $('#minutePB').css('width', strip + '%').attr('aria-valuenow', strip);
+        interval = setInterval(updateMinute, 1000);
+    } else {
+        document.querySelector('#StartEnd').innerHTML = 'START GAME';
+        $('#sideNavBar').css('pointer-events', 'auto');
+        $('#eventTypeSelectAddEvent').empty();
+        $('#teamSelectAddEvent').empty();
+        clearInterval(interval);
+        finalReport();
+    }
+});
+
+function finalReport() {
+
+}
+
+function updateMinute() {
+    if (minute >= 89) clearInterval(interval);
+    minute += 1;
+    document.querySelector('#minute').innerHTML = minute.toString() + "'";
+    strip += 1;
+    $('#minutePB').css('width', strip + '%').attr('aria-valuenow', strip);
 }
 
 var GameOptions = $();
@@ -375,6 +421,12 @@ var GameOptionID;
 let game_id;
 
 $('#refereeButton').click(function () {
+    $("#leaguesView").css("display", "none");
+    $("#seasonsView").css("display", "none");
+    $("#TeamsView").css("display", "none");
+    $("#GamesView").css("display", "none");
+    $("#RefereeView").css("display", "none");
+    $("#RefView").css("display", "block");
     let refereeOptions = [];
     refereeOptions[0] = new Object();
     refereeOptions[0].referee_id = email;
@@ -385,17 +437,29 @@ $('#refereeButton').click(function () {
         GameOptions = $();
         data.forEach(game => {
             GameOptions = (createGameOption(game));
+            GameOptions.click(function () {
+                document.querySelector(".selected").innerHTML = GameOptions.find("label").html();
+                GameOptionID = GameOptions.find("label").attr('for');
+                document.querySelector(".option-container").classList.remove("active");
+            });
             $('#gameOptions').append(GameOptions);
         });
     }).catch(function (data) {
         console.log("request failed");
+    });
+    minute = 0;
+    strip = 10;
+    document.querySelector('#minute').innerHTML = minute + "'";
+    $('#minutePB').css('width', strip + '%').attr('aria-valuenow', strip);
+    document.querySelector(".selected").addEventListener("click", () => {
+        document.querySelector(".option-container").classList.toggle("active");
     });
 });
 
 $("#selectGame").click(function () {
     let gameID = [];
     gameID[0] = new Object();
-    // gameID[0].game_id = GameOptionID.substr(10);
+    gameID[0].game_id = GameOptionID.substr(10);
     let SecureObj = new SecurityObj(email, "1000", "startGame", gameID);
     postSend("/Referee/getEventProperties", SecureObj).then(function (data) {
         var eventType = document.getElementById("eventTypeSelectAddEvent");
@@ -407,19 +471,53 @@ $("#selectGame").click(function () {
         var teamSelector = document.getElementById("teamSelectAddEvent");
         var option = document.createElement("option");
         option.text = data[1].home;
+        document.querySelector('#homeTeam').innerHTML = data[1].home;
         teamSelector.add(option);
         var option = document.createElement("option");
         option.text = data[1].away;
+        document.querySelector('#awayTeam').innerHTML = data[1].away;
         teamSelector.add(option);
     }).catch(function (data) {
         console.log(data);
     });
     $('#RefView').hide();
     $('#RefereeView').removeAttr('hidden');
+    $('#RefereeView').css('display', 'block');
+    $('#HomeEvents').empty();
+    $('#AwayEvents').empty();
 });
 
-$("#addGameEvent").click(function () {
-    let minute = document.getElementById("GameMinute").value;
+function createHomeEvent(data) {
+    let homeEvent = [
+        '<div>',
+        data.minute || 'undefined',
+        ' - ',
+        data.eventType || 'undefined',
+        ' - ',
+        data.description || 'undefined',
+        '</div>'
+    ];
+    return $(homeEvent.join(''));
+}
+
+function createAwayEvent(data) {
+    let awayEvent = [
+        '<div>',
+        data.eventType || 'undefined',
+        ' - ',
+        data.description || 'undefined',
+        ' - ',
+        data.minute || 'undefined',
+        '</div>'
+    ];
+    return $(awayEvent.join(''));
+}
+
+let homeGoals = 0;
+let awayGoals = 0;
+
+$("#addEvent").click(function (event) {
+    event.preventDefault();
     let team = document.getElementById("teamSelectAddEvent").value;
     let eventType = document.getElementById("eventTypeSelectAddEvent").value;
     let description = document.getElementById("description").value;
@@ -435,5 +533,59 @@ $("#addGameEvent").click(function () {
     //postSendWithoutReturn("/app/Referee/addEventToGame", SecureObj);
     stompClient.send("/app/chat", {}, JSON.stringify(SecureObj));
     //console.log("done!!!");
+    let emptyEvent = $('<div><br></div>');
+    if (team == document.querySelector('#homeTeam').innerHTML) {
+        if (eventType == 'goal') {
+            homeGoals += 1;
+            $('#score').html(homeGoals + ' : ' + awayGoals);
+        }
+        let event = createHomeEvent(eventProperties[0]);
+        $('#HomeEvents').append(event);
+        $('#AwayEvents').append(emptyEvent);
+    } else {
+        if (eventType == 'goal') {
+            awayGoals += 1;
+            $('#score').html(homeGoals + ' : ' + awayGoals);
+        }
+        let event = createAwayEvent(eventProperties[0]);
+        $('#AwayEvents').append(event);
+        $('#HomeEvents').append(emptyEvent);
+    }
 });
 
+function createAlert(alert) {
+    let alertTemplate = [
+        '<div class="alertUpdate"><h5>',
+        alert.date,
+        ' - ',
+        alert.description,
+        '</h5></div>'
+    ];
+    return $(alertTemplate.join(''));
+}
+
+$("#alertsButton").click(function () {
+    let gameID = [];
+    gameID[0] = new Object();
+    let SecureObj = new SecurityObj(email, "1000", "getAlerts", gameID);
+    postSend("/Fan/getUpdates", SecureObj).then(function (data) {
+        let alerts = [];
+        let content = "";
+        $('#NewAlertsContent').empty();
+        for (let i = 0; i < data.length; i++) {
+            alerts[i] = new Alert(data[0].description, data[0].date, data[0].readed);
+            $('#NewAlertsContent').append(createAlert(alerts[i]));
+        }
+        console.log(content);
+        setAlertsRead();
+    }).catch(function (data) {
+        console.log(data);
+    });
+});
+
+function setAlertsRead() {
+    let gameID = [];
+    gameID[0] = new Object();
+    let SecureObj = new SecurityObj(email, "1000", "setAllAlertsReaded", gameID);
+    postSend("/Fan/setUpdatesReaded", SecureObj);
+}
